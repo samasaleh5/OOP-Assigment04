21eed34cc52b0936516e8da3143da2cad0c8c838
samasaleh5 <ss2104@fayoum.edu.eg>
2025-01-17 15:44:07 +0200

    part 2 Question1 solved

--- OOP-Assigment04/Program.cs
+++ OOP-Assigment04/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace OOP_Assigment04
+﻿using OOP_Assigment04.Interface;
+
+namespace OOP_Assigment04
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            //Part01
             #region Question1
             //1-b
             //2-a,b
@@ -15,6 +18,13 @@
             //8-b
             //9-d
             //10-c
+            #endregion
+            //Part02
+            #region Question1
+            ICircle circle = new Circle(5);
+            circle.DisplayShapeInfo();
+            IRectangle rectangle = new Rectangle(7, 9);
+            rectangle.DisplayShapeInfo();
             #endregion
         }
     }
