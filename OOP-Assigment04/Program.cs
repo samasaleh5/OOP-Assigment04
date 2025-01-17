5f4ca69ee4143f6c6e5b1dec95af4a0ffbda0272
samasaleh5 <ss2104@fayoum.edu.eg>
2025-01-17 15:05:00 +0200

    question1 solved

--- OOP-Assigment04/Program.cs
+++ OOP-Assigment04/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question1
+            //1-b
+            //2-a,b
+            //3-c
+            //4-b
+            //5-non the of the above c# use : operator
+            //6-a
+            //7-b
+            //8-b
+            //9-d
+            //10-c
+            #endregion
         }
     }
 }
